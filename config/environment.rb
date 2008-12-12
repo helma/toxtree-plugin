a617d96cc4712d9b783f43f3f9b8e80c1c8cfd78
Christoph Helma <helma@in-silico.de>
2008-12-12 17:00:06 +0100

    scripts added

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 
 # R and Java configuration files 
-require "R.rb"
+#require "R.rb"
 require "java.rb"
 
 Rails::Initializer.run do |config|
@@ -70,7 +70,7 @@ end
 
 # Include your application configuration below
 
-ENV['CLASSPATH'] = "#{RAILS_ROOT}/lib/java/:#{RAILS_ROOT}/lib/java/toxtree/toxTree/dist/Toxtree-1.60.jar:#{RAILS_ROOT}/vendor/plugins/opentox/lib/java/cdk-1.1.svn-19012008.jar"
+ENV['CLASSPATH'] = "#{RAILS_ROOT}/lib/java/:#{RAILS_ROOT}/lib/java/toxtree/toxTree/dist/Toxtree-1.60.jar:#{RAILS_ROOT}/lib/java/cdk-1.1.svn-19012008.jar"
 ENV['PATH'] =  "#{RAILS_ROOT}/vendor/bin:" + ENV['PATH']
 ENV['GEM_HOME'] = "#{RAILS_ROOT}/vendor/lib/ruby/gems/1.8"
 ENV['GEM_PATH'] = "#{RAILS_ROOT}/vendor/lib/ruby/gems/1.8"
